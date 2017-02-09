51f4ba20d7e553c173e19feabda9975c9fe32b3b
Ayush Gupta <ayush@theguptas.net>
2017-02-09 07:35:06 -0800

    Add files via upload

--- file.rb
+++ file.rb
@@ -1,7 +1,30 @@
-file = File.open("sample.txt") {|f| f.read.split}
-file.map! do |word|
-    word.gsub(/[\.\! ]/, "")
-    word.downcase
-end
+class MarkovChainGenerator
+    def initialize(filename)
+        @file = File.open(filename) {|f| f.read.split}
+        @file.map! do |word|
+            word.gsub(/[\.\! ]/, "")
+            word.downcase
+        end
+        @file << ""
+    end
 
-p file
+    def generate(len)
+        @current_word = @file[0]
+    end
+
+    private
+
+    def get_list
+        @succeeding_words = {}
+        index = 0
+        while index < @file.length
+            if @file[index] == @current_word
+                if @succeeding_words.has_key?(@file[index])
+                    @succeeding_words[@file[index]] = @succeeding_words.fetch(@file[index]) + 1
+                else
+                    @succeeding_words[@file[index]] = 1
+                end
+        end
+        return @succeeding_words
+    end
+end
